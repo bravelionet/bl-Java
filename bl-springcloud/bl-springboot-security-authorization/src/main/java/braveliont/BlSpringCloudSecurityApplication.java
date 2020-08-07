7b790c02a9b6cbb2543b5ad787c9f95df3143ea3
lionet <bravelionet@163.com>
2020-08-07 11:47:29 +0800

    # 增加 Security授权案例模块 # 调整 Security认证目录文件

---
+++ bl-springcloud/bl-springboot-security-authorization/src/main/java/braveliont/BlSpringCloudSecurityApplication.java
@@ -0,0 +1,11 @@
+package braveliont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlSpringCloudSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlSpringCloudSecurityApplication.class);
+    }
+}
