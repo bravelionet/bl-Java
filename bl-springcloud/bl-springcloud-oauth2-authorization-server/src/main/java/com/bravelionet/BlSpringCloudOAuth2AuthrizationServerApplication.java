b4d7527790fe612f36090951f6de69f16444683c
lionet <bravelionet@163.com>
2020-08-11 00:56:29 +0800

    简单实现 SpringSecurity OAuth2 授权码模式 -> 修改 bl-springcloud-oauth2-authorization-server 服务配置类

--- bl-springcloud/bl-springcloud-oauth2-authorization-server/src/main/java/com/bravelionet/BlSpringCloudOAuth2AuthrizationServerApplication.java
+++ bl-springcloud/bl-springcloud-oauth2-authorization-server/src/main/java/com/bravelionet/BlSpringCloudOAuth2AuthrizationServerApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(value = {"com.bravelionet"})
+@EnableFeignClients(basePackages = {"com.bravelionet"})
 public class BlSpringCloudOAuth2AuthrizationServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlSpringCloudOAuth2AuthrizationServerApplication.class,args);
